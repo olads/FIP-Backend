629dae2662a08b1d4202b469fb5d7b1e4b7f785b
harwo <stephenmigia@gmail.com>
2025-01-16 08:43:46 +0100

    Week 1 deliverable submission

---
+++ src/main/java/com/migia/Flexisaf/SetupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.migia.Flexisaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetupProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/migia/Flexisaf/SetupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migia.Flexisaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
